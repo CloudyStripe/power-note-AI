12f8141330d64b56b8ca1be12d5d2826dc50f65f
Skyler Savard <ssavard@korterra.com>
2023-09-24 20:27:32 -0500

    side panel established. JEST config issues fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import './app.css'
 
 export const App = () => {
   return (
-    <div className="container">
-      <h1>Hello</h1>
+    <div id="container">
+      <h1>Power Note AI</h1>
+      <textarea id="raw_note" placeholder="Insert notes..."></textarea>
+      <button>Submit</button>
+      <textarea id="organized_note" placeholder="Insert notes..."></textarea>
     </div>
   )
 }
--- src/_tests/_App.test.tsx
+++ src/_tests/_App.test.tsx
@@ -3,7 +3,11 @@ import { App } from '../App'
 
 describe('App', () => {
     it('renders without crashing', () => {
+
         renderComponent()
+
+        const el = document.getElementById('container')
+        expect(el).not.toBeNull()
     })
 })
 
