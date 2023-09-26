d7dcd132046fc1c0802c0dc4d50138a352a1023f
Skyler Savard <ssavard@korterra.com>
2023-09-25 21:59:49 -0500

    basic organization.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ export const App = () => {
   return (
     <div id="container">
       <h1>Power Note AI</h1>
-      <textarea id="raw_note" placeholder="Insert notes..."></textarea>
+      <textarea className="note" placeholder="Insert notes..."></textarea>
       <button>Submit</button>
-      <textarea id="organized_note" placeholder="Insert notes..."></textarea>
+      <textarea className="note" placeholder="Generate notes..."></textarea>
     </div>
   )
 }
