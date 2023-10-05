e6d693f0ef76b16afa3239ad57046a00406b0161
Skyler Savard <ssavard@korterra.com>
2023-10-04 21:34:23 -0500

    properly functioning. still needs a unit test.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import './app.scss'
 
 export const App = () => {
 
-
   const [userNotes, setUserNotes] = useState<string | null>(null)
-  const [generatedNotes, setGeneratedNotes] = useState<string | null>(null)
+  const [generatedNotes, setGeneratedNotes] = useState<string>('')
 
   const submitNotes = async () => {
     const connection = await fetch('http://localhost:3001/organize', {
@@ -32,14 +31,18 @@ export const App = () => {
 
   return (
     <div id="container">
-      <h1>Power Note AI</h1>
+      <h1 className="header">Power Note AI</h1>
       <textarea
         className="note"
         onChange={(e) => { setUserNotes(e.target.value) }}
         placeholder="Insert notes..."></textarea>
-      <button onClick={() => submitNotes()}>Submit</button>
-      <textarea className="note" placeholder="Generate notes..."></textarea>
-      <div dangerouslySetInnerHTML={{ __html: generatedNotes! }}></div>
+      <button className="submitBtn" onClick={() => submitNotes()}>Submit</button>
+      <div 
+        className="note noteResult" 
+        dangerouslySetInnerHTML={{ __html: generatedNotes! }} 
+        placeholder="Generate notes..."
+        contentEditable={true}
+      />
     </div>
   )
 }
