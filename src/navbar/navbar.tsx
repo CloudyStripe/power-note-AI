dd6056623f6765b26cc2fe10bf4f4ea62c1c51dd
Skyler Savard <ssavard@korterra.com>
2023-12-19 14:53:45 -0600

    modal added and styling complete. still need tests.

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -1,17 +1,58 @@
-import { SettingOutlined } from '@ant-design/icons';
+import { HighlightOutlined, LogoutOutlined, SettingOutlined } from '@ant-design/icons';
 import './navbar.scss'
+import { useState } from 'react';
+import { Menu, MenuProps, Modal } from 'antd';
 
 export const Nav: React.FC = () => {
 
+    const [settingsOpen, setSettingsOpen] = useState<boolean>(false)
+
+    const items: MenuProps['items'] = [
+        {
+            label: 'Highlight & Export',
+            key: 'highlight-export',
+            icon: <HighlightOutlined />,
+            children: [
+                {
+                    label: 'On',
+                    key: 'setting:1',
+                },
+                {
+                    label: 'Off',
+                    key: 'setting:2',
+                },
+            ],
+        },
+        { type: 'divider' },
+        {
+            label: 'Sign Out',
+            key: 'sign-out',
+            icon: <LogoutOutlined />
+
+        },
+    ];
+
     return (
         <div className="navContainer">
-            <img 
-                className="logo" 
+            <img
+                className="logo"
                 src="/images/logo_dark.png"
             />
             <SettingOutlined
+                onClick={() => setSettingsOpen(!settingsOpen)}
                 className="settingsIcon"
             />
+            <Modal
+                className="settingsModal"
+                footer={null}
+                onCancel={() => setSettingsOpen(false)}
+                open={settingsOpen}
+                title="Settings">
+                <Menu 
+                    mode='inline' 
+                    items={items}>
+                </Menu>
+            </Modal>
         </div>
     )
 }
